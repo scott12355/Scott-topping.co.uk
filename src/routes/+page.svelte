5509eee79a910f7d3ad509b200fd7134722d96e0
scott12355 <scott.r.topping@gmail.com>
2024-10-01 16:44:59 +0100

    github & linkedin icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<script>
+<script lang="ts">
 	import Home from '$lib/home.svelte';
+
+	import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
+
+	import FaGithub from 'svelte-icons/fa/FaGithub.svelte';
 </script>
 
 <body>
@@ -10,4 +14,48 @@
 		</header>
 	</div>
 	<Home></Home>
+
+	<div class="flex flex-row gap-5 place-content-center">
+		<div>
+			<a
+				class="w-1 h-1"
+				href="https://github.com/scott12355"
+				aria-label="GitHub"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<div class="icon">
+					<FaGithub />
+				</div>
+			</a>
+		</div>
+
+		<div>
+			<a
+				href="https://www.linkedin.com/in/scotttopping/"
+				aria-label="Linkedin"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<div class="icon">
+					<FaLinkedin />
+				</div>
+			</a>
+		</div>
+	</div>
 </body>
+
+<style>
+	a {
+		color: white;
+		text-decoration: none;
+	}
+	.icon {
+		cursor: pointer;
+		transition: color 0.2s ease-in-out;
+		width: 40px;
+	}
+	.icon:hover {
+		color: #4c25b6;
+	}
+</style>
