673fee3d1af1c528886a4ff4bd8e487d49a4a9fb
scott12355 <scott.r.topping@gmail.com>
2024-10-08 14:04:57 +0100

    Remove Supabase variables into env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,4 @@
-  import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_KEY, SUPABASE_ENDPOINT } from '$env/static/private';
 
-  export const supabase = createClient('https://zdhtjvturkvwyzjidtrd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkaHRqdnR1cmt2d3l6amlkdHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzNzk2NTMsImV4cCI6MjA0Mzk1NTY1M30.x70rdqOfbuIN9nE4gJk3EUOKpwr5CW323tP-41PFmAo')
+export const supabase = createClient(SUPABASE_ENDPOINT, SUPABASE_KEY);
