254f38b02ecb211d8308edac6ecd7c5a84c20eb1
scott12355 <scott.r.topping@gmail.com>
2024-11-14 16:23:05 +0000

    Update +page.svelte

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,16 +8,19 @@
 
 	<div class="flex-col justify-center items-center space-y-4 p-9">
 		<p>
-			Welcome to my chatbot project! This is my first foray into chatbot development, built in
-			Python using hugging-face spaces to host this model. For the purpose of cheap hosting, this
-			particular chatbot is very light weight.
+			Welcome to my chatbot project! This is my first look into NLP chatbot development. Built with
+			Python using and using hugging-face spaces to host this model. For the purpose of cheap
+			hosting, this particular chatbot is very light weight and so prone to hallucinations.
 		</p>
 		<p>
-			Beyond its conversational capabilities, the chatbot incorporates a sentiment analysis
-			pipeline, allowing it to gauge the tone of your messages and respond thoughtfully.\n This is
-			just the beginning of my exploration into the exciting field of conversational AI. I plan to
-			continue building on this foundation, enhancing my skills and creating more innovative tools
-			in the future. Thank you for trying it out!
+			This chatbot uses the Qwen2.5-1.5B-Instruct model. Additionally, my chatbot incorporates a
+			sentiment analysis pipeline, allowing it to gauge the tone of your messages and respond
+			thoughtfully.
+		</p>
+		<p>
+			This is just the beginning of my exploration into the exciting field of conversational AI. I
+			plan to continue building on this foundation, enhancing my skills and creating more innovative
+			tools in the future. Thank you for trying it out!
 		</p>
 	</div>
 </div>
